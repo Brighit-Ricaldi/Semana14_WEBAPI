1d13950d59a188528f20b995527cc1a4bf00e84a
Brighit-Ricaldi <brighit.ricaldi@tecsup.edu.pe>
2021-06-16 00:01:02 -0500

    Semana14-WEB-API

---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using MVCAJAX.Models;
+using System.Threading.Tasks;
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        Proxy.StudentProxy proxy = new Proxy.StudentProxy();
+       
+
+        public ActionResult IndexRazor()
+        {
+            var response = Task.Run(()=>proxy.GetStudentsAsync());
+            return View(response.Result.listado);
+        }
+
+        
+        public JsonResult getStudent(string id)
+        {
+            var response = Task.Run(() => proxy.GetStudentsAsync());
+
+            return Json(response.Result.listado, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult createStudent(StudentModel std)
+        {
+            var response = Task.Run(() => proxy.InsertAsync(std));
+            string message = response.Result.Mensaje;
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
